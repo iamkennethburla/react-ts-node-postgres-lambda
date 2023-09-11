05545752d601c532b1e30e9394262cbbc3e50129
iamkennethburla <kenneth.burla@gmai.com>
2023-09-12 00:42:50 +0800

    test hello route

--- server-main/src/main.ts
+++ server-main/src/main.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
